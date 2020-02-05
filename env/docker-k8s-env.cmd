6361203f4d0f7f13489b1e5e02c604649d0f770d
Anton de Swardt <anton.deswardt@gmail.com>
2020-02-05 09:13:06 +0000

    first commit

---
+++ env/docker-k8s-env.cmd
@@ -0,0 +1,5 @@
+SET DOCKER_TLS_VERIFY=1
+SET DOCKER_HOST=tcp://192.168.99.102:2376
+SET DOCKER_CERT_PATH=C:\Users\Prashini Naidoo\.minikube\certs
+REM Run this command to configure your shell:
+REM @FOR /f "tokens=*" %i IN ('minikube docker-env') DO @%i
